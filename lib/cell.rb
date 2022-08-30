66889af983d32bb2e376c6cc4cb560a39e843109
Aether Kerstens <philippekerstens@gmail.com>
2022-08-30 14:03:32 -0700

    Add methods to cell

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,7 +1,25 @@
 class Cell
-    attr_reader :coordinate, :ship
-    def initialize(coordinate)
-        @coordinate = coordinate
-        @ship 
+  attr_reader :coordinate, :ship, :fired_upon
+  def initialize(coordinate)
+    @coordinate = coordinate
+    @ship = nil
+    @fired_upon = false
+  end
+  def empty?
+    if @ship == nil
+      true
+    else
+      false
     end
+  end
+  def place_ship(boat_name)
+    @ship = boat_name
+  end
+  def fired_upon?
+    @fired_upon
+  end
+  def fire_upon
+    @ship.hit 
+    @fired_upon = true
+  end
 end
