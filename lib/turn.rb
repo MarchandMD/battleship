fccef9a33d7ef03fbce1d12e57cbd8e7451eb7fc
Michael Marchand <michaeldavidmarchand@gmail.com>
2023-01-21 16:40:49 -0700

    Rubcop lintin

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,20 +8,19 @@ class Turn
     @cruiser = Ship.new("Cruiser", 3)
     @submarine = Ship.new("Submarine", 2)
     @coord_array = %w[A1 A2 A3 A4 B1 B2 B3 B4 C1 C2 C3 C4 D1 D2 D3 D4]
-
   end
 
   def ai_ship_placement
     loop do
       x = @coord_array.shuffle
       # binding.pry
-      if @ai.valid_placement?(@cruiser, x[3..5])
-        @ai.place(@cruiser, x[3..5])
-        # @ai.place(@submarine, x[5..6])
-        x.shift(3)
-        # binding.pry
-        break
-      end
+      next unless @ai.valid_placement?(@cruiser, x[3..5])
+
+      @ai.place(@cruiser, x[3..5])
+      # @ai.place(@submarine, x[5..6])
+      x.shift(3)
+      # binding.pry
+      break
     end
   end
 end
--- runner.rb
+++ runner.rb
@@ -1,3 +1,3 @@
-require './lib/game.rb'
+require './lib/game'
 
 game = Game.new.start
