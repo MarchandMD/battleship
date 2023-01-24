4f2aa2f9cdafbe1c18a544d792acdd6582469adf
Michael Marchand <michaeldavidmarchand@gmail.com>
2023-01-24 14:19:20 -0700

    updates game_spec to remove unnecessary tests

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,17 +10,5 @@ class Turn
     @coord_array = %w[A1 A2 A3 A4 B1 B2 B3 B4 C1 C2 C3 C4 D1 D2 D3 D4]
   end
 
-  def ai_ship_placement
-    loop do
-      x = @coord_array.shuffle
-      # binding.pry
-      next unless @ai.valid_placement?(@cruiser, x[3..5])
-
-      @ai.place(@cruiser, x[3..5])
-      # @ai.place(@submarine, x[5..6])
-      x.shift(3)
-      # binding.pry
-      break
-    end
-  end
+  
 end
